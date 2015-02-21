545cdf836bcc785fcb8abcd85a75aa80510e3b6b
gfitzy123 <gfitzgerald123@gmail.com>
2015-02-20 21:20:18 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,58 @@
-
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+   
+var newGame = function(){
+	return Math.floor(Math.random() * 100) + 1;
+}
 
-  	});
+var randomNumber = newGame();
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-  
+var guessCount = 0;
 
 
-
-	$(".new").click(function() {
-		// create random number
-	var theNumber = Math.floor(Math.random() * 100) + 1;
-		//set guesses to zero
-	$("#count").html(0)
-	});		
+$(".new").click(function() {
+	guessCount = 0
+	$("#count").html(guessCount)
+	$("#guessList li").remove();
+});
 
 
-	$("#userGuess").submit(function(x) {
-		 if ( x >= theNumber - 10 && x <= theNumber + 10 ) {
-				$("h2 #feedback").html("<h2> Very hot </h2>");
-			} else if (x >= theNumber - 20 && x <= theNumber + 20 ) {
-				$("h2 #feedback").html("<h2> Hot </h2>");
-			} else if (x >= theNumber - 40 && x <= theNumber + 40) {
-				$("h2 #feedback").html("<h2> Cold </h2>");
-			} else if (x >= theNumber - 60 && x <= theNumber + 60) {
-				$("h2 #feedback").html("<h2> Very cold </h2>");
-			}
-	}
+
+var checkNumber = function(randomNumber, userNumber){
+	var difference = Math.abs(randomNumber - userNumber)
+	if (difference <= 10) {
+		return "Very hot";
+	} else if (difference <= 20) {
+		return "Hot";
+	} else if (difference <= 40) {
+		return "Cold";
+	} else if (difference <= 60) {
+		return "Very cold";
+	} else return "Super cold";
+}
+
+$("#guessButton").click(function() {
+	var userNumber = $("#userGuess").val();
+	console.log(userNumber);
+	var result = checkNumber(randomNumber, userNumber);
+	$("#feedback").text(result);
+	guessCount = guessCount + 1
+	$("#count").html(guessCount);
+	$("#guessList").prepend("<li>" + userNumber + "</li>");
 
 });
+
+
+
+/*--- Display information modal box ---*/
+$(".what").click(function(){
+	$(".overlay").fadeIn(1000);
+
+});
+
+/*--- Hide information modal box ---*/
+$("a.close").click(function(){
+	$(".overlay").fadeOut(1000);
+});
+  
+});
+
